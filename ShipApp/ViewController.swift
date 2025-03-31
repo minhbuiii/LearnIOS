05bccfc7e2f8f72231ba36d7a8e6ee94c160eebe
Minh Bùi <bminh248@gmail.com>
2025-03-31 16:20:18 +0700

    Đẩy code ios

---
+++ ShipApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShipApp
+//
+//  Created by Bùi Minh on 31/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShipAppTests/ShipAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShipAppTests.swift
+//  ShipAppTests
+//
+//  Created by Bùi Minh on 31/3/25.
+//
+
+import Testing
+@testable import ShipApp
+
+struct ShipAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
