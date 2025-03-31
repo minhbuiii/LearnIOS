f3dc830de3f2ef573b10276d3b633e631ed5a267
Minh Bùi <bminh248@gmail.com>
2025-03-31 16:56:09 +0700

    Update code

--- ShipApp/ViewController.swift
+++ ShipApp/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ship: UIImageView!
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "spaceshipfly", ofType: "wav")
+        let url = URL(fileURLWithPath: path!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
-
+    @IBAction func onButtonClicked(_ sender: Any) {
+        self.audioPlayer.play()
+        self.ship.isHidden = false
+        UIView.animate(withDuration: 3, animations: {
+            self.ship.frame = CGRect(x: 50, y: 300, width: 300, height: 200 )
+        })
+    }
+    
 }
 
